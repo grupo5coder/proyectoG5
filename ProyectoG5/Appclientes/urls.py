c786327d59ff9968c4c05101179db620686f84e2
grupo5coder <grupo5coder@gmail.com>
2022-06-07 17:41:44 -0300

    corregido

--- ProyectoG5/Appclientes/urls.py
+++ ProyectoG5/Appclientes/urls.py
@@ -5,12 +5,12 @@ from django.urls import path
 
 from ProyectoG5.views import index
 
-from Appclientes.views import Client_view, new_client_view
+from Appclientes.views import client_view, new_client_view
 
 urlpatterns = [
 
 
-    path("",Client_view , name = "Client_view"),
+    path("",client_view , name = "client_view"),
 
     path("create-client/",new_client_view, name = "new_client_view"),
 
